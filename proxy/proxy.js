b491373b8963cf0ab6601556c93ec26630d44d80
joeskib <146056538+sdfsdfasdfsdfasdfsdfasdf@users.noreply.github.com>
2024-12-21 16:04:12 -0600

    Delete proxy directory

--- proxy/proxy.js
+++
@@ -1,8 +0,0 @@
-                function go() {
-                    var input = document.getElementById('searchbar');
-                    if (input.value) {
-                        window.location.href = '/main/' + input.value;
-                    } else {
-                        alert('Please provide a link. It does not require https://.');
-                    }
-                }
